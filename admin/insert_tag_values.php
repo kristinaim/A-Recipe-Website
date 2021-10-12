1c8be233eb8a5a02d5c692d79d8f3d28ca21503e
Jake Day <jday@scu.edu>
2021-10-12 13:17:33 -0700

    Add Tag class and values to insert

---
+++ admin/insert_tag_values.php
@@ -0,0 +1,22 @@
+<?php
+require_once "../src/tag.php";
+
+$tag = new Tag();
+$json_str = '[{"descr":"Meal Prep Friendly","descr_short":"MPF"},
+              {"descr":"Quick and Easy","descr_short":"Q&E"}]';
+
+/*      
+$params = array(array("descr"=>"Meal Prep Friendly",
+                      "descr_short"=>"MPF"),
+                array("descr"=>"Quick and Easy",
+                      "descr_short"=>"Q&E"),
+                array("descr"=>"Healthy",
+                      "descr_short"=>"H"),
+                array("descr"=>"Keto",
+                      "descr_short"=>"K"),
+                array("descr"=>"Pescetarian",
+                      "descr_short"=>"P"),
+                array(*/
+$json_obj = json_decode($json_str, true);
+echo $json_obj;
+?>
---
+++ src/tag.php
@@ -0,0 +1,9 @@
+<?php
+require_once "entry.php";
+
+class Tag extends Entry {
+  public function __construct() {
+    parent::__construct("tag");
+  }
+}
+?>
