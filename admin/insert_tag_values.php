953da7cbe743028e1d5c6b5703510636d1af7ad5
Jake Day <jday@scu.edu>
2021-10-12 14:04:00 -0700

    Demo tag insert

--- admin/insert_tag_values.php
+++ admin/insert_tag_values.php
@@ -3,26 +3,21 @@ require_once "../src/tag.php";
 
 $tag = new Tag();
 $json_str = '[{"descr":"Meal Prep Friendly","descr_short":"MPF"},
-              {"descr":"Quick and Easy","descr_short":"Q&E"}]';
+              {"descr":"Quick and Easy","descr_short":"Q&E"},
+              {"descr":"Health","descr_short":"H"},
+              {"descr":"Keto","descr_short":"K"},
+              {"descr":"Pescetarian","descr_short":"P",
+              {"descr":"Vegetarian","descr_short":"V",
+              {"descr":"Vegan","descr_short","VE"}]';
 
-/*      
-$params = array(array("descr"=>"Meal Prep Friendly",
-                      "descr_short"=>"MPF"),
-                array("descr"=>"Quick and Easy",
-                      "descr_short"=>"Q&E"),
-                array("descr"=>"Healthy",
-                      "descr_short"=>"H"),
-                array("descr"=>"Keto",
-                      "descr_short"=>"K"),
-                array("descr"=>"Pescetarian",
-                      "descr_short"=>"P"),
-                array(*/
 $json_obj = json_decode($json_str, true);
 
 foreach($json_obj as $row) {
+  echo $tag->insert($row);
+  /*
   foreach($row as $key => $value) {
     echo $key . " => " . $value . "<br>";
-  }
+  }*/
 }
 
 ?>
