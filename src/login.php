4319445fc545bee64216e2ee6209a300476f64a0
Jake Day <jday@scu.edu>
2021-10-12 17:25:35 -0700

    Change variable name to reflect data type change

--- src/login.php
+++ src/login.php
@@ -34,8 +34,7 @@ function create_user($email, $first_name, $last_name, $password) {
 function login($email, $password) {
   $user = new User();
   $exists = $user->select(["email" => $email], "s");
-  $json_arr = json_decode($exists, true);
-  
-  return isset($json_arr[0]) && password_verify($password, $json_arr[0]->password_hash);
+  $json_obj = json_decode($exists);
+  return isset($json_obj[0]) && password_verify($password, $json_obj[0]->password_hash);
 }
 ?>
