4694f206c9296f7f82942adac058afe561d7cc4b
Jake Day <jday@scu.edu>
2021-10-14 17:50:57 -0700

    Add view for recipe, add instrucitons to recipe display page

---
+++ src/recipe_instruction_view.php
@@ -0,0 +1,9 @@
+<?php
+require_once "view.php";
+
+class RecipeInstructionView extends View {
+  public function __construct() {
+    parent::__construct("recipe_instruction_vw");
+  }
+}
+?>
--- web/display/recipe.php
+++ web/display/recipe.php
@@ -1,7 +1,7 @@
 <?php
-require_once(__DIR__."/../../src/recipe.php");
-require_once(__DIR__."/../../src/recipe_category.php");
+require_once(__DIR__."/../../src/reciep_view.php");
 require_once(__DIR__."/../../src/recipe_ingredient_view.php");
+require_once(__DIR__."/../../src/recipe_instruction_view.php");
 session_start();
 
 // verify user is logged in
@@ -9,17 +9,20 @@ if (!$_SESSION["login"]) {
   header("Location: " . "../web/login.php");
 }
 
-$recipe = new Recipe();
-$select = $recipe->select(["recipe_id" => $_GET["id"]], "i");
+// get recipe and category
+$recipe_vw = new RecipeView();
+$select = $recipe_vw->select(["recipe_id" => $_GET["id"]], "i");
 $recipe_obj = json_decode($select)[0];
 
-$recipe_cat = new RecipeCategory();
-$select = $recipe_cat->select(["recipe_category_id" => $recipe_obj->recipe_category_id], "i");
-$recipe_cat_obj = json_decode($select)[0];
-
+// get recipe ingredients
 $recipe_ingr_vw = new RecipeIngredientView();
 $select = $recipe_ingr_vw->select(["recipe_id" => $recipe_obj->recipe_id], "i");
 $recipe_ingr_obj = json_decode($select);
+
+// get recipe instructions
+$recipe_instr_vw = new RecipeInstructionView();
+$select = $recipe_instr_vw->select(["recipe_id" => $recipe_obj->recipe_id], "i");
+$recipe_instr_obj = json_decode($select);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -32,11 +35,16 @@ $recipe_ingr_obj = json_decode($select);
     echo "Recipe ID: " . $recipe_obj->recipe_id . "<br>";
     echo "Name: " . $recipe_obj->name . "<br>";
     echo "Serving Size: " . $recipe_obj->serving_size . "<br>";
-    echo "Category: " . $recipe_cat_obj->category . "<br>";
+    echo "Category: " . $recipe_obj->category . "<br>";
     
     echo "Ingredients:" . "<br>";
     foreach($recipe_ingr_obj as $ingr) {
-      echo $ingr->amount . " " . $ingr->ingredient . "<br>";
+      echo "- " . $ingr->amount . " " . $ingr->ingredient . "<br>";
+    }
+    
+    echo "Instructions:" . "<br>";
+    foreach($recipe_instr_obj as $instr) {
+      echo $instr->step_index+1 . "). " . $instr->step . "<br>";
     }
     ?>
 	</body>
