2bf46fdb701ae6845b9c6dd61ad28a8aaf0b3030
Jake Day <jday@scu.edu>
2021-10-16 15:56:31 -0700

    Add root file for directory structure

---
+++ root.php
@@ -0,0 +1,9 @@
+<?php
+define("DIR_BASE", dirname(__FILE__) . "/");
+define("DIR_CSS", DIR_BASE . "css/");
+define("DIR_SRC", DIR_BASE . "src/");
+define("DIR_WEB", DIR_BASE . "web/");
+define("DIR_LOC", DIR_BASE . "local/");
+define("DIR_TST", DIR_BASE . "test/");
+define("DIR_ADM", DIR_BASE . "admin/");
+?>
