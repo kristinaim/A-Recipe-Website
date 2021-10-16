978ad60eccf9e80711ee01e9e527cd1f33e7fa81
unknown <jaked97@gmail.com>
2021-10-16 16:05:40 -0700

    Add CSS file, test directory constants

--- src/header.php
+++ src/header.php
@@ -1,30 +1,31 @@
+<?php require_once(__DIR__."/../root.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
-  <link rel="stylesheet" href="../css/styles.css" type="text/css">
+  <!--link rel="shortcut icon" href=<?=DIR_IMG?>"favicon.ico" type="image/x-icon"-->
+  <link rel="stylesheet" href=<?=DIR_CSS?>"styles.css" type="text/css">
   <title>A Recipe Website</title>
 </head>
 <body>
   <header>
     <ul class="nav">
       <li>
-        <a href="../web/display/home.php">Home</a>
+        <a href=<?=DIR_WEB?>"display/home.php">Home</a>
       </li>
       <li>
-        <a href="../web/display/Recipes.php">Recipes</a>
+        <a href=<?=DIR_WEB?>"display/Recipes.php">Recipes</a>
       </li>
       <li>
-        <a href="../web/display/favorites.php">Favorites</a>
+        <a href=<?=DIR_WEB?>"display/favorites.php">Favorites</a>
       </li>
       <li>
-        <a href="../web/display/submission.php">Add a Recipe</a>
+        <a href=<?=DIR_WEB?>"display/submission.php">Add a Recipe</a>
       </li>
       <li>
-        <a href="logout.php">Logout</a>
+        <a href=<?=DIR_WEB?>"logout.php">Logout</a>
       </li>
     </ul>
   </header>
