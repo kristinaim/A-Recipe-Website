0ad9c49d8cbe06712c2d51cd05e1a102c5079f0f
Jake Day <jday@scu.edu>
2021-10-19 10:39:06 -0700

    Add favorite/unfavorite functionality

--- js/scripts-main.js
+++ js/scripts-main.js
@@ -13,57 +13,50 @@ function decodeHtml(html) {
   return txt.value;
 }
 
-function toggleStar(currentValue, starElement, recipeElement) {
+function toggleStar(currentValue, starElement, recipeId) {
   const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", starElement.getAttribute("action"));
+  xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  
   let fav = 0;
-  let request;
-
   if (currentValue == decodeHtml(starOn)) {
     starElement.innerHTML = starOff;
   } else {
     starElement.innerHTML = starOn;
     fav = 1;
   }
-
   starElement.setAttribute("fav", fav);
-  request = starElement.getAttribute("action")
-            +"?recipe_id="
-            +recipeElement.getAttribute("recipe-id")
-            +"&fav="+fav;
-  xhttp.open("GET", request);
-  xhttp.send();
+  
+  xhttp.send("function=set_favorite&recipe_id="+recipeId+"&fav="+fav);
 }
 
-function isFavorite(action, recipe_id, callback) {
+function isFavorite(action, recipeId, callback) {
   const xhttp = new XMLHttpRequest();
   xhttp.open("POST", action);
   xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
   xhttp.onload = function() {
     callback(parseInt(this.responseText));
   }
-  xhttp.send("function=is_favorite&recipe_id="+recipe_id);
+  xhttp.send("function=is_favorite&recipe_id="+recipeId);
 }
 
 function onRecipeLoad() {
   const recipeTitle = document.getElementById("recipeTitle");
   const favoriteStar = document.getElementById("favoriteStar");
 
-  let favorited;
-
   // exit if neither exist
   if (recipeTitle === null || favoriteStar === null) {
     return;
   }
 
-  // TODO: change to async!!!
   // https://stackoverflow.com/a/16825593
-  isFavorite((favoriteStar.getAttribute("action"),
-              recipeTitle.getAttribute("recipe-id"),
-              favorited) => {
+  const action = favoriteStar.getAttribute("action");
+  const recipeId = recipeTitle.getAttribute("recipe-id"); 
+  isFavorite(action, recipeId, favorited => {
     favoriteStar.innerHTML = (favorited==true) ? starOn : starOff;
   });
 
   favoriteStar.addEventListener("click", (event) => {
-    toggleStar(event.target.innerHTML, favoriteStar, recipeTitle);
+    toggleStar(event.target.innerHTML, favoriteStar, recipeId);
   });
 }
--- src/user_favorite.php
+++ src/user_favorite.php
@@ -6,12 +6,7 @@ require_once __DIR__."/../root.php";
 require_once DIR_SRC."favorite.php";
 require_once DIR_SRC."favorite_recipe_view.php";
 session_start();
-// verify user is logged in
-/*
-if (!isset($_SESSION["login"])) {
-  header("Location: ".LINK_WEB."login.php");
-}
- */
+
 // check if the displayed recipe is favorited
 function is_favorited($recipe_id, $user_id) {
   $params = ["recipe_id" => $recipe_id,
@@ -21,10 +16,27 @@ function is_favorited($recipe_id, $user_id) {
   return !empty($select);
 }
 
+// favorite or unfavorite a recioe
+function set_favorite($recipe_id, $user_id, $is_favorite) {
+  $params = ["recipe_id" => $recipe_id,
+             "user_id" => $user_id];
+  $favorite = new Favorite();
+
+  // insert or delete
+  if ($is_favorite) {
+    $favorite->insert($params, "ii");
+  } else {
+    $favorite->remove($params, "ii");
+  }
+}
+
 if (isset($_POST["function"])) {
   switch ($_POST["function"]) {
     case "is_favorite":
       echo is_favorited($_POST["recipe_id"], $_SESSION["user"]);
+      break;
+    case "set_favorite":
+      set_favorite($_POST["recipe_id"], $_SESSION["user"], $_POST["fav"]);
       break;
     default:
       echo 0;
