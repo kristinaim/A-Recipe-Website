daf8a90c5dfc1c6e94955610091bedc525f96d54
kristinaim <51719404+kristinaim@users.noreply.github.com>
2021-10-19 19:15:02 -0700

    Update footer.php

--- src/footer.php
+++ src/footer.php
@@ -1,11 +1,12 @@
+<!DOCTYPE html>
+<html lang = "en">
+<body>
     <footer>
-      <div class="footer">
-        <ul>
-          <li>Phone: 123 456 7890</li>
-          <li>Fax: 555 555 1234</li>
-          <li>Email: email@email.com</li>
-        </ul>
-      </div>
+        <div class = "footer">
+            <ul>
+                <li>Contact us for more info at <b><a href = "mailto:contact@homemade.com">contact@homemade.com</a></b></li>
+            </ul>
+        </div>
     </footer>
-  </body>
+</body>
 </html>
